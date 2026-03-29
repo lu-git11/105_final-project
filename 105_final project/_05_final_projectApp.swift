c9285dc94409812bda8728a60a0930fab15aa1f2
Jeffrey Lullen <lullenj@gmail.com>
2026-03-29 13:47:50 +0800

    Initial Commit

---
+++ 105_final project/_05_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _05_final_projectApp.swift
+//  105_final project
+//
+//  Created by jeffrey lullen on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct _05_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
